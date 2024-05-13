9df6fdf536e81adca9a01cc58a524b61d1df53fd
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-05-13 15:52:41 -0300

    Corrigida ordem dos providers

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,30 @@
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BDNaMemoriaService } from './app/bdna-memoria.service';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import {
+    withInterceptorsFromDi,
+    provideHttpClient,
+} from '@angular/common/http';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientInMemoryWebApiModule.forRoot(BDNaMemoriaService, { dataEncapsulation: false }), NgbModule, FormsModule),
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-  .catch(err => console.error(err));
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(
+            BrowserModule,
+            AppRoutingModule,
+            HttpClientInMemoryWebApiModule.forRoot(BDNaMemoriaService, {
+                dataEncapsulation: false,
+            }),
+            NgbModule,
+            FormsModule
+        ),
+    ],
+}).catch((err) => console.error(err));
