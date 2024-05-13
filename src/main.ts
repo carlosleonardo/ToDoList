17366cc57dc120700091033fd45f66b1f58cf938
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-05-13 15:50:07 -0300

    Atualizado o bootstrap para usar standalone

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,22 @@
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BDNaMemoriaService } from './app/bdna-memoria.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientInMemoryWebApiModule.forRoot(BDNaMemoriaService, { dataEncapsulation: false }), NgbModule, FormsModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
