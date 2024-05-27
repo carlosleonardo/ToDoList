5633fc3a4c5b46067fb94356464c75086005add9
Carlos Leonardo Goes Farias <carlosleonardogoes@outlook.com>
2024-05-27 19:35:03 -0300

    Alterada forma de busca de tarefa para usar ngModel

--- src/app/buscador/buscador.component.ts
+++ src/app/buscador/buscador.component.ts
@@ -1,28 +1,32 @@
 import { Component, inject, output } from '@angular/core';
 import { FiltroService } from '../filtro.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-buscador',
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './buscador.component.html',
     styleUrl: './buscador.component.css',
 })
 export class BuscadorComponent {
     filtroService = inject(FiltroService);
     aoBuscar = output<string>();
+    texto = '';
 
-    buscar(texto: string) {
-        if (texto) {
+    buscar() {
+        if (this.texto) {
             this.filtroService.adicionarFiltro({
                 nome: 'buscar-nome',
-                valor: texto,
+                valor: this.texto,
                 funcaoFiltro: (tarefa) =>
-                    tarefa.nome.toLowerCase().includes(texto.toLowerCase()),
+                    tarefa.nome
+                        .toLowerCase()
+                        .includes(this.texto.toLowerCase()),
             });
         } else {
             this.filtroService.removerFiltro('buscar-nome');
         }
-        this.aoBuscar.emit(texto);
+        this.aoBuscar.emit(this.texto);
     }
 }
